f9ffe9775be3f583b9ba323cd7b43989959ea5ac
Sukanta1304 <sukantadolai97@gmail.com>
2022-05-30 16:15:50 +0530

    1

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
+import axios from "axios"
+
 
 const Product = () => {
+  const [Product, setProduct] = useState([])
   // TODO: Remove below const and instead import them from chakra
   const Text = () => <div />;
   const Image = () => <div />;
@@ -9,6 +12,14 @@ const Product = () => {
   const Heading = () => <div />;
   const Tag = () => <div />;
   const TagLabel = () => <div />;
+
+  useEffect(()=>{
+    const getdata= async()=>{
+      let res= await axios.get("http://localhost:8080/products")
+      console.log(res)
+    }
+  getdata() ;  
+  },[])
   return (
     <Stack data-cy="product">
       <Image data-cy="product-image" />
